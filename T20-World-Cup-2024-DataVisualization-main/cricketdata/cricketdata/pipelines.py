bcc6bb0a06c39cb10720464f9a68e823fcdff73c
Samvid Bharambe <117520358+Samvid2003@users.noreply.github.com>
2024-08-05 21:25:23 +0530

    Add files via upload

---
+++ T20-World-Cup-2024-DataVisualization-main/cricketdata/cricketdata/pipelines.py
@@ -0,0 +1,5 @@
+# Define your item pipelines here
+
+class CricketdataPipeline:
+    def process_item(self, item, spider):
+        return item
---
+++ T20-World-Cup-2024-DataVisualization-main/cricketdata/cricketdata/settings.py
@@ -0,0 +1,8 @@
+# Scrapy settings for cricketdata project
+
+BOT_NAME = 'cricketdata'
+
+SPIDER_MODULES = ['cricketdata.spiders']
+NEWSPIDER_MODULE = 'cricketdata.spiders'
+
+ROBOTSTXT_OBEY = True
